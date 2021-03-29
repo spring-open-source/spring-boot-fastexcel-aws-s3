ebae5d5a9d6f78ac8e344e7a5df04b65f1682f02
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-03-29 17:39:22 +0530

    initial project commit

---
+++ src/main/java/com/hardik/flash/SpringBootFastexcelAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.hardik.flash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFastexcelAwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFastexcelAwsS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/flash/SpringBootFastexcelAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.flash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFastexcelAwsS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
